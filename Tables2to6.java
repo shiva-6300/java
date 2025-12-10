0f9b15073ca1b7acf48f3753fc54df99f508c684
shiva-6300 <vaddishiva51@gmail.com>
2025-12-10 11:41:18 +0530

    added java file

---
+++ Tables2to6.java
@@ -0,0 +1,14 @@
+public class Tables2to6 {
+    public static void main(String[] args) {
+        
+        for (int i = 2; i <= 6; i++) {        // Loop for tables 2 to 6
+            System.out.println("Table of " + i + ":");
+            
+            for (int j = 1; j <= 10; j++) {   // Loop for each table (1 to 10)
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            
+            System.out.println(); // Blank line after each table
+        }
+    }
+}
