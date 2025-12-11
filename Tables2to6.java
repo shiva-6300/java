4662aed4ab872468146be0d2929d902bd1a49292
Shiva <vaddishiva51@gmail.com>
2025-12-11 12:25:54 +0530

    modified

--- Tables2to6.java
+++ Tables2to6.java
@@ -1,10 +1,10 @@
 public class Tables2to6 {
     public static void main(String[] args) {
         
-        for (int i = 5; i <= 10; i++) {        // Loop for tables 2 to 6
+        for (int i = 2; i <= 6; i++) {        // Loop for tables 2 to 6
             System.out.println("Table of " + i + ":");
             
-            for (int j = 1; j <= 10; j++) {   // Loop for each table (1 to 10)
+            for (int j = 1; j <= 5; j++) {   // Loop for each table (1 to 10)
                 System.out.println(i + " x " + j + " = " + (i * j));
             }
             
